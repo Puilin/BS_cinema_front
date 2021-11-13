7d5ccdb2019d91a38d5b4516ed05aad02f0b48ed
Puilin <suryblue@naver.com>
2021-11-13 16:34:22 +0900

    index.js 수정

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Frame from './components/frame';
-import NoticeInfo from './components/index_/noticeinfo';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
