b58edae88cf7070167ce993bb9d559d14b293b1f
Puilin <suryblue@naver.com>
2021-11-24 19:02:38 +0900

    로그인 구현

--- src/components/admin_/admin.js
+++ src/components/admin_/admin.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Admin_login from './admin_login';
 
 class Admin extends Component {
     render() {
@@ -14,10 +15,10 @@ class Admin extends Component {
                             <div class="row">
                                 <div class="col-md-12">
                                     <div class="hero-ct">
-                                        <h1 width="100px">로그인</h1>
+                                        <h1 width="100px">관리자 페이지</h1>
                                         <ul class="breadcumb">
                                             <li class="active">
-                                                <a href="#">홈</a>
+                                                <a href="#">관리자</a>
                                             </li>
                                             <li>
                                                 <span class="ion-ios-arrow-right"></span>
@@ -28,61 +29,7 @@ class Admin extends Component {
                             </div>
                         </div>
                     </div>
-                    <div class="movie-tabs">
-                        <div class="tabs">
-                            <ul class="tab-links tabs-mv">
-                                <div class="login-wrapper" id="signup-content">
-                                    <div class="login-content">
-                                        <h3>로그인</h3>
-                                        <br/>
-                                        <div>
-                                            <form method="post" 
-                                            class="form-style-1"
-                                            action="#">
-                                                <div 
-                                                
-                                                class="row">
-                                                    <label 
-                                                    style={style_input}
-                                                    for="username-2">
-                                                        아이디:
-                                                        <input
-                                                            type="text"
-                                                            name="username"
-                                                            class="username-2"
-                                                            placeholder="아이디"
-                                                            pattern="^[a-zA-Z][a-zA-Z0-9-_\.]{8,20}$"
-                                                            required="required"/>
-                                                    </label>
-                                                </div>
-                                                <div 
-                                                class="row">
-                                                    <label 
-                                                    style={style_input}
-                                                    for="password-2">
-                                                        비밀번호:
-                                                        <input
-                                                            type="password"
-                                                            name="password"
-                                                            id="password-2"
-                                                            placeholder="비밀번호"
-                                                            pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"
-                                                            required="required"/>
-                                                    </label>
-                                                </div>
-                                                <br/>
-                                                <div class="row">
-                                                    <button 
-                                                    style={style_input}
-                                                    class="redbtn" type="submit">로그인</button>
-                                                </div>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </div>
-                            </ul>
-                        </div>
-                    </div>
+                    <Admin_login></Admin_login>
                 </div>
         );
     }
