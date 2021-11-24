13f2107879f2fe51d9d9da84854925e311764ffd
Jiwoo <jxxwxxx@gmail.com>
2021-11-24 19:05:38 +0900

    관리자 페이지

--- src/components/admin_/admin.js
+++ src/components/admin_/admin.js
@@ -1,89 +1,97 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Admin_content from './admin_content';
 
 class Admin extends Component {
-    render() {
-        const style_input={
-            width: '450px',
-            margin: '20px'
+    constructor(props) {
+        super(props);
+        this.state = {
+            tab: "sales",
+            in_detail: false
         }
-
+    }
+    render() {
         return (
-            <div class="buster-light">
+            <div>
+                <div class="buster-light">
                     <div class="hero common-hero">
                         <div class="container">
                             <div class="row">
                                 <div class="col-md-12">
                                     <div class="hero-ct">
-                                        <h1 width="100px">로그인</h1>
+                                        <h1 width="100px">관리자 페이지</h1>
                                         <ul class="breadcumb">
                                             <li class="active">
                                                 <a href="#">홈</a>
                                             </li>
                                             <li>
                                                 <span class="ion-ios-arrow-right"></span>
-                                                로그인</li>
+                                                관리자</li>
                                         </ul>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="movie-tabs">
-                        <div class="tabs">
-                            <ul class="tab-links tabs-mv">
-                                <div class="login-wrapper" id="signup-content">
-                                    <div class="login-content">
-                                        <h3>로그인</h3>
-                                        <br/>
-                                        <div>
-                                            <form method="post" 
-                                            class="form-style-1"
-                                            action="#">
-                                                <div 
-                                                
-                                                class="row">
-                                                    <label 
-                                                    style={style_input}
-                                                    for="username-2">
-                                                        아이디:
-                                                        <input
-                                                            type="text"
-                                                            name="username"
-                                                            class="username-2"
-                                                            placeholder="아이디"
-                                                            pattern="^[a-zA-Z][a-zA-Z0-9-_\.]{8,20}$"
-                                                            required="required"/>
-                                                    </label>
-                                                </div>
-                                                <div 
-                                                class="row">
-                                                    <label 
-                                                    style={style_input}
-                                                    for="password-2">
-                                                        비밀번호:
-                                                        <input
-                                                            type="password"
-                                                            name="password"
-                                                            id="password-2"
-                                                            placeholder="비밀번호"
-                                                            pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"
-                                                            required="required"/>
-                                                    </label>
-                                                </div>
-                                                <br/>
-                                                <div class="row">
-                                                    <button 
-                                                    style={style_input}
-                                                    class="redbtn" type="submit">로그인</button>
-                                                </div>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </div>
-                            </ul>
-                        </div>
+                </div>
+                <div class="movie-tabs">
+                    <div class="tabs">
+                        <ul class="tab-links tabs-mv">
+                            <li>
+                                <a
+                                    href="#overview"
+                                    onClick={function (e) {
+                                        e.preventDefault();
+                                        this.setState({tab: "sales", in_detail: false});
+                                    }.bind(this)}>매출현황</a>
+                            </li>
+                            <li>
+                                <a
+                                    href="#overview"
+                                    onClick={function (e) {
+                                        e.preventDefault();
+                                        this.setState({tab: "pay", in_detail: false});
+                                    }.bind(this)}>급여 관리</a>
+                            </li>
+                            <li>
+                                <a
+                                    href="#overview"
+                                    onClick={function (e) {
+                                        e.preventDefault();
+                                        this.setState({tab: "benefit_analysis", in_detail: false});
+                                    }.bind(this)}>수익 분석</a>
+                            </li>
+                            <li>
+                                <a
+                                    href="#overview"
+                                    onClick={function (e) {
+                                        e.preventDefault();
+                                        this.setState({tab: "board", in_detail: false});
+                                    }.bind(this)}>게시판 관리</a>
+                            </li>
+                            <li>
+                                <a
+                                    href="#overview"
+                                    onClick={function (e) {
+                                        e.preventDefault();
+                                        this.setState({tab: "movie", in_detail: false});
+                                    }.bind(this)}>영화 정보 관리</a>
+                            </li>
+                            <li>
+                                <a
+                                    href="#overview"
+                                    onClick={function (e) {
+                                        e.preventDefault();
+                                        this.setState({tab: "product", in_detail: false});
+                                    }.bind(this)}>상품 관리</a>
+                            </li>
+                        </ul>
+                        <Admin_content
+                            tab={this.state.tab}
+                             
+                            ></Admin_content>
                     </div>
                 </div>
+            </div>
         );
     }
 }
